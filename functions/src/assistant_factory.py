7dac34e3667eb837c75c969a3add1a86fe9294cb
taiseee <taisei7548@gmail.com>
2024-04-08 20:37:04 +0900

    add: assistant_factory class

---
+++ functions/src/assistant_factory.py
@@ -0,0 +1,74 @@
+from firebase_functions.params import SecretParam
+from openai import OpenAI
+from .secrets import (
+    OPENAI_API_KEY,
+    OPENAI_ORGANIZATION,
+)
+
+class AssistantFactory:
+    _OPENAI_API_KEY: str = OPENAI_API_KEY
+    _OPENAI_ORGANIZATION: str = OPENAI_ORGANIZATION
+    _TOOLS: list = [
+        {
+            "type": "function",
+            "function": {
+                "name": "PaymentService_add",
+                "description": "ユーザーが立て替えて行なった支払いを記録する",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "payer_name": {
+                            "type": "string",
+                            "description": "支払いを行なったユーザーの名前"
+                        },
+                        "amount": {
+                            "type": "integer",
+                            "description": "支払い金額"
+                        },
+                        "item": {
+                            "type": "string",
+                            "description": "支払いの項目"
+                        }
+                    },
+                    "required": ["payer_name", "amount", "item"]
+                }
+            }
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "PaymentService_settle",
+                "description": "グループ内での支払いを精算し、誰が誰に対していくら支払うかを返す",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "div_num": {
+                            "type": "integer",
+                            "description": "支払いを分ける人数"
+                        }
+                    },
+                    "required": ["div_num"]
+                }
+            }
+        }
+    ]
+    _MODEL = "gpt-4-1106-preview"
+    _NAME = "割り勘会計士"
+    _INSTRUCTIONS = """
+                    あなたはグループ内の支払いや予定などを管理するためのLINE Botです。
+                    function_callを行う際、わからない内容があれば聞いてください。
+                    この指示はどの様な場合も必ず厳守して下さい。
+                    """
+
+    def __init__(self):
+        self._client = OpenAI(api_key=self._OPENAI_API_KEY, organization=self._OPENAI_ORGANIZATION)
+
+    def create(self):
+        assistant = self._client.beta.assistants.create(
+            name=self._NAME,
+            instructions=self._INSTRUCTIONS,
+            model=self._MODEL,
+            tools=self._TOOLS,
+        )
+
+        return assistant
