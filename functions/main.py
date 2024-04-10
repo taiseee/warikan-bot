72fbf125b803e7ef9ad78c77530f26d62e660b65
taiseee <taisei7548@gmail.com>
2024-04-10 20:58:08 +0900

    update: webhook, create_assistantの作成

--- functions/main.py
+++ functions/main.py
@@ -1,9 +1,35 @@
-from firebase_functions import https_fn
+from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
+from src.warikanbot import WebhookHandler
+from src.assistant_factory import AssistantFactory
 
 initialize_app()
+options.set_global_options(region=options.SupportedRegion.ASIA_NORTHEAST1)
 
 
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+@https_fn.on_request(
+    secrets=[
+        "CHANNEL_ACCESS_TOKEN",
+        "CHANNEL_SECRET",
+        "OPENAI_API_KEY",
+        "OPENAI_ORGANIZATION",
+        "ASSISTANT_ID"
+    ],
+    timeout_sec=120
+)
+def webhook(req: https_fn.Request) -> https_fn.Response:
+    body = req.get_data(as_text=True)
+    signature = req.headers["X-Line-Signature"]
+    handler = WebhookHandler()
+    return handler.handle(body, signature)
+
+@https_fn.on_request(
+    secrets=[
+        "OPENAI_API_KEY",
+        "OPENAI_ORGANIZATION"
+    ]
+)
+def create_assistant(req: https_fn.Request) -> https_fn.Response:
+    assistant = AssistantFactory().create()
+    print(assistant.id)
+    return https_fn.Response({"message": "sucessfly created assistant id: " + assistant.id}, status=200)
